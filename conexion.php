aabf775b3cd1ad8c09cc43ad25a9099f3ca019d2
nocive.science <nocive.science@gmail.com>
2023-11-27 03:58:53 -0300

    version para practicar desde cero

--- conexion.php
+++ conexion.php
@@ -1,18 +1,3 @@
 <?php
-    $conexion= mysqli_connect("localhost", "root", "", "prueba");
-    if(isset($_POST['submit'])){
-        if(!empty($_POST['nombre'])
-            && !empty($_POST['apellido'])
-            && !empty($_POST['ciudad'])
-        ){
-            $name=$_POST['nombre'];
-            $surname=$_POST['apellido'];
-            $city=$_POST['ciudad'];
-            $consulta="INSERT INTO real_one(name, surname, city) VALUES ('$name', '$surname', '$city')";
-            $resultado=mysqli_query($conexion, $consulta);
-            if($resultado){
-                
-            }
-        }
-    }
+    
 ?>
